e6fd09dae44789282f0b661f4921f6632ae13e71
jhsaraja <jaana.h.sarajarvi@gmail.com>
2018-08-31 12:23:26 +0300

    Changed requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [
+    'requests>=2.7.0',
+    'pycrypto>=2.6.1',
+]
 
 setup_requirements = [ ]
 
