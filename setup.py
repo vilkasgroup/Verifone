1d749689f94280722cf3889ab2af5da662c0458b
jhsaraja <jaana.h.sarajarvi@gmail.com>
2018-09-03 09:01:55 +0300

    Updated pipfile

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/jhsaraja/verifone',
+    url='https://github.com/vilkasgroup/Verifone',
     version='0.1.0',
     zip_safe=False,
 )
