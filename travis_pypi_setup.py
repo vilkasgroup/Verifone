c2b8330919a16da5e26d6c3f3523074a8d03f21b
jhsaraja <jaana.h.sarajarvi@gmail.com>
2018-09-12 14:54:06 +0300

    Updated requirements

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'vilkasgroup/shipfunk_python'
+GITHUB_REPO = 'vilkasgroup/Verifone'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
