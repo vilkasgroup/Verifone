2b221138bf1207399183527168a5b007c0e2ca90
jhsaraja <jaana.h.sarajarvi@gmail.com>
2019-04-01 09:47:29 +0300

    Updated dev requirements

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='verifone',
-    version='0.1.12',
+    version='0.1.13',
     description="Python package for Verifone",
     long_description=readme + '\n\n' + history,
     author="Jaana Sarajärvi",
