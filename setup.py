f6a93bcd91f7cd48c8450c8e25edd2aaa5e1e1b2
jhsaraja <jaana.h.sarajarvi@gmail.com>
2019-04-15 11:06:48 +0300

    Updated Jinja2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='verifone',
-    version='0.1.14',
+    version='0.1.15',
     description="Python package for Verifone",
     long_description=readme + '\n\n' + history,
     author="Jaana Sarajärvi",
