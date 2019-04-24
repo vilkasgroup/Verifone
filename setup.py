ad35e6ad946397c8f0c454710e64b73906b4d212
jhsaraja <jaana.h.sarajarvi@gmail.com>
2019-04-24 15:02:04 +0300

    modified logging

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='verifone',
-    version='0.1.16',
+    version='0.1.17',
     description="Python package for Verifone",
     long_description=readme + '\n\n' + history,
     author="Jaana Sarajärvi",
